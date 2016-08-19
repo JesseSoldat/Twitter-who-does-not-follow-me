a2f1c4e65b8c81ac6a6a11aa127fd5c686c9f744
jesse soldat <jsoldat@hotmail.com>
2016-08-19 15:39:36 +0700

    can see how many followers there are

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var Twitter = require('twitter');
-
+ 
 var client = new Twitter({
-	consumer_key: '3A98ovHBVZMmp1Gza874HrwYk',
-	consumer_secret: '	sM3VVuLJ7apCVWlyWhhJLkbLxqt4XQjTqpXzBtkxEEdOu1jocv',
-	access_token_key: '4275177635-aPHVn0rQJeaBGIPFfdEJ7FKLf0IVw1abmjtdUBY',
-	access_token_secret: '	eUqSGurjZsxTn19zTUJmJJ3T0j4boQ8lpBmOGqLv047yy'
+  consumer_key: '3A98ovHBVZMmp1Gza874HrwYk',
+  consumer_secret: 'sM3VVuLJ7apCVWlyWhhJLkbLxqt4XQjTqpXzBtkxEEdOu1jocv',
+  access_token_key: '4275177635-aPHVn0rQJeaBGIPFfdEJ7FKLf0IVw1abmjtdUBY',
+  access_token_secret: 'eUqSGurjZsxTn19zTUJmJJ3T0j4boQ8lpBmOGqLv047yy'
+});
+ 
+var params = {screen_name: 'jkup'};
+client.get('followers/ids', params, function(error, users, response) {
+  if (!error) {
+    console.log(users.ids.length);
+  }
 });
