1649521ac1aada905968f8d6fd46c88797b97a8f
jesse soldat <jsoldat@hotmail.com>
2016-08-19 15:23:36 +0700

    first

---
+++ app.js
@@ -0,0 +1,8 @@
+var Twitter = require('twitter');
+
+var client = new Twitter({
+	consumer_key: '3A98ovHBVZMmp1Gza874HrwYk',
+	consumer_secret: '	sM3VVuLJ7apCVWlyWhhJLkbLxqt4XQjTqpXzBtkxEEdOu1jocv',
+	access_token_key: '4275177635-aPHVn0rQJeaBGIPFfdEJ7FKLf0IVw1abmjtdUBY',
+	access_token_secret: '	eUqSGurjZsxTn19zTUJmJJ3T0j4boQ8lpBmOGqLv047yy'
+});
